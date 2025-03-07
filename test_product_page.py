0f6c4cfdd925a469c706f5f611b8125f0c915262
Rubtsov.AA <lexun2@yandex.ru>
2025-03-07 09:31:29 +0300

    add parametrize for test_guest_can_add_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -1,20 +1,14 @@
 from .pages.product_page import ProductPage
 import time, pytest
 
-@pytest.mark.parametrize("promo",[
-                                    "?promo=offer0",
-                                    "?promo=offer1",
-                                    "?promo=offer2",
-                                    "?promo=offer3",
-                                    "?promo=offer4",
-                                    "?promo=offer5",
-                                    "?promo=offer6",
-                                    pytest.param("?promo=offer7", marks=pytest.mark.xfail),
-                                    "?promo=offer8",
-                                    "?promo=offer9",                                    
-                                    ])
+# @pytest.mark.parametrize("promo",[
+#                                     0, 1, 2, 3, 4, 5, 6,
+#                                     pytest.param(7, marks=pytest.mark.xfail),
+#                                     8, 9                                   
+#                                     ])
+@pytest.mark.parametrize("promo",[ num if num!=7 else pytest.param(num, marks=pytest.mark.xfail) for num in range(10) ])
 def test_guest_can_add_product_to_basket(browser, promo):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{promo}"
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo}"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_basket()
