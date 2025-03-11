32941a8b70ca8310353e041aed3e0570dee3677d
Rubtsov.AA <lexun2@yandex.ru>
2025-03-11 23:10:49 +0300

    bug fix 'test_guest_cant_see_product_in_basket_opened_from_product_page'

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -6,13 +6,13 @@ from .base_page import BasePage
 class BasketPage(BasePage):
     
     def should_not_be_items_in_basket(self):
-        self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS)
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), "Element 'BASKET_ITEMS' is visible!"
 
     def should_be_message_basket_empty(self):
-        self.is_element_present(*BasketPageLocators.BASKET_EMPTY)
+        assert self.is_element_present(*BasketPageLocators.BASKET_EMPTY), "Element 'BASKET_EMPTY' is not visible!"
 
     def should_be_items_in_basket(self):
-        self.is_element_present(*BasketPageLocators.BASKET_ITEMS)
+        assert self.is_element_present(*BasketPageLocators.BASKET_ITEMS), "Element 'BASKET_ITEMS' is not visible!"
 
     def should_not_be_message_basket_empty(self):
-        self.is_not_element_present(*BasketPageLocators.BASKET_EMPTY)
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_EMPTY), "Element 'BASKET_EMPTY' is visible!"
