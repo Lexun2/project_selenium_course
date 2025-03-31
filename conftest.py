5c9cddfb46472c73e50275eeaf11ee0d35336c43
Rubtsov.AA <lexun2@yandex.ru>
2025-03-31 08:16:50 +0300

    update init browser, delete CromeDriverManager

--- conftest.py
+++ conftest.py
@@ -15,7 +15,7 @@ def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='ru, en', help="Choose language: 'ru' or 'en'")
 
 def browser_chrome_settings(request):
-    chrome_service = ChromeService(ChromeDriverManager().install())
+    # chrome_service = ChromeService(ChromeDriverManager().install())   # в новой версии селениума обновили инициализацию, менеджер больше не нужен
     options = Options()
     user_language = request.config.getoption("language")
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
@@ -27,7 +27,8 @@ def browser_chrome_settings(request):
         print("Запущен в headless режиме")
     else:
         print("Запущен в обычном режиме")
-    browser = webdriver.Chrome(service = chrome_service, options = options)
+    # browser = webdriver.Chrome(service = chrome_service, options = options)  # так было с менеджером
+    browser = webdriver.Chrome(options = options)
     return browser
 
 def browser_firefox_settings(request):
